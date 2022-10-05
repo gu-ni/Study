2bb5d1e9e02cc0557e9863a9378cb7bcd8baa952
Taenam Park <taenam@inha.edu>
2022-10-06 01:53:33 +0900

    modify

---
+++ Algorithm/step1.py
@@ -0,0 +1,24 @@
+# %%
+## 2557 : Hello World
+print("Hello World!")
+# %%
+## 10718 : We love kriii
+for i in range(2):
+    print("강한친구 대한육군")
+# %%
+## 1000 : A+B
+a = list(map(int, input().split()))
+print(a[0] + a[1])
+# %%
+## 1001 : A-B
+a = list(map(int, input().split()))
+print(a[0] - a[1])
+# %%
+## 10998 : AxB
+a = list(map(int, input().split()))
+print(a[0] * a[1])
+# %%
+## 1008 : A/B
+a = list(map(int, input().split()))
+print(a[0] / a[1])
+# %%
